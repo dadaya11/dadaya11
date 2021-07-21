870d48aac7e71fe770a5850de2d7a7aa9960e8a1
dadaya11 <87759784+dadaya11@users.noreply.github.com>
2021-07-21 16:07:51 +0500

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,13 @@
+VANTA.WAVES({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  shininess: 77.00,
+  waveHeight: 22.50,
+  waveSpeed: 1.45
+})
